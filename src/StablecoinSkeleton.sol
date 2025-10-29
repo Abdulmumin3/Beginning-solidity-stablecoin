ef26c75d1f94bc66ae3c88daede93718b1e53684
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2025-10-29 15:17:35 +0100

    Added depositCollateral functions

--- src/StablecoinSkeleton.sol
+++ src/StablecoinSkeleton.sol
@@ -19,7 +19,7 @@ contract StablecoinSkeleton is ReentrancyGuard {
     uint256 constant LIQUIDATION_THRESHOLD = 50; // 50%
 
     mapping(address token => address priceFeed) s_priceFeed;
-    mapping(address user => mapping(address token => uint256 amount)) s_collateralDeposited;
+    mapping(address user => mapping(address token => uint256 amount)) s_collateralDeposit;
     mapping(address user => uint256 amountSBTMinted) s_SBTMinted;
 
     address[] s_collateralTokens;
@@ -53,7 +53,52 @@ contract StablecoinSkeleton is ReentrancyGuard {
     }
 
     function depositCollateralAndMintSBT(address tokenCollateralAddress, uint256 amountCollateral, uint256 amountSBTtoMint) external {
-        // depositCollateral(tokenCollateralAddress, amountCollateral);
-        // mintSBT(amountSBTtoMint);
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintSBT(amountSBTtoMint);
+    }
+
+    function depositCollateral(address _tokenCollateralAddress, uint256 _amountCollateral) public collateralMoreThanZero(_amountOfCollateral) isAllowedToken(_tokenCollateralAddress) nonReentrant {
+        s_collateralDeposit[msg.sender][_tokenCollateralAddress] += _amountCollateral;
+        emit CollateralDeposited(msg.sender, _tokenCollateralAddress, _amountCollateral);
+        bool success = IERC20(_tokenCollateralAddress).transferFrom(msg.sender, address(this), _amountCollateral);
+        if(!success) {
+            revert StablecoinSkeleton_TransferFailed();
+        }
+    }
+
+    function redeemCollateralForSBT(address tokenCollateralAddress, uint256 amountCollateral, uint256 amountSBTtoburn) external {
+        burnSBT(amountSBTtoburn);
+        redeemCollateral(tokenCollateralAddress, amountCollateral);
+    }
+
+    function redeemCollateral(address tokenCollateralAddress, uint256 amountOfCollateral) public collateralMoreThanZero(amountOfCollateral) nonReentrant {
+        s_collateralDeposit[msg.sender][tokenCollateralAddress] -= amountOfCollateral;
+        emit CollateralRedeemed(msg.sender, tokenCollateralAddress, amountOfCollateral);
+        bool success = IERC20(tokenCollateralAddress).transfer(msg.sender, amountOfCollateral);
+        if(!success) {
+            revert StablecoinSkeleton_TransferFailed();
+        }
+    }
+
+    function mintSBT(uint256 amountSBTToBeMinted) public collateralMoreThanZero(amountSBTToBeMinted) nonReentrant {
+        s_SBTMinted[msg.sender] += amountSBTToBeMinted;
+        revertIfHealthFactorDoesNotWork(msg.sender);
+        bool minted = i_stablecoin.mint(msg.sender, amountSBTToBeMinted);
+
+        if(!minted) {
+            revert StablecoinSkeleton__MintingFailed();
+        }
+    }
+
+    function burnSBT(uint256 _amount) public collateralMoreThanZero(_amount) {
+        s_SBTMinted[msg.sender] -= amount;
+        bool success = i_stablecoin.transferFrom(msg.sender, address(this), _amount);
+        if(!success){
+            revert StablecoinSkeleton_TransferFailed();
+        }
+
+        i_stablecoin.burn(_amount);
+
+        revertIfHealthFactorDoesNotWork(msg.sender);
     }
 }
